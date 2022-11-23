314cb0da42003673adc7c1e82a3c42d22eabf790
Juthisarker <tanudas71.21@gmail.com>
2022-11-23 15:19:44 +0600

    pure functions added

---
+++ namste_dev/prototype/prototype.js
@@ -0,0 +1,11 @@
+let arr = ["Akshay",  "Aditya" ];
+let obj = {
+    name : "akshay",
+    city:"dehradun",
+    getintro : function(){
+        console.log(this.name+"from"+this.city);
+    }
+}
+console.log(arr);
+
+
